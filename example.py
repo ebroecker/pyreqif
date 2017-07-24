64a786d0e8af9d741cbe2c668dd64cb18c3f1db9
ebroecker <eduard@gmx.de>
2017-07-24 22:56:05 +0200

    update example to work with example from reqifstudio

--- example.py
+++ example.py
@@ -2,15 +2,18 @@
 
 import os.path
 import pyreqif.reqif
+import pyreqif.rif
 
-filename = "aa.xml"
+
+filename = "/home/edu/Downloads/ReqIFStudio/workspace/reqifTest/Spec.reqif"
 basepath = os.path.dirname(filename)
 
 
 def workOnHierarch(asd, deepth = 0):
     for i in range(0, deepth):
         print "  ",
-    print asd["Object ID"]
+    print asd["ReqIF.ForeignID"],
+    print asd["ReqIF.Text"]
     if "attachments" in asd:
         for attachment in asd["attachments"]:
             absPath = os.path.abspath(attachment)
@@ -20,13 +23,17 @@ def workOnHierarch(asd, deepth = 0):
 
 
 myDoc = pyreqif.reqif.load(filename)
+f = open("bb.xml", "wb")
+pyreqif.rif.dump(myDoc,f)
 
 
 print "Specification Requirements:"
 specs = myDoc.asDict()
 for spec in specs:
   for req in spec:
-      print req["Object ID"]
+      print req["ReqIF.ForeignID"],
+      print req["ReqIF.Text"]
+
 
 print "Specification Requirements (hierarchical):"
 hierarchSpec = myDoc.asHierarchDict()
