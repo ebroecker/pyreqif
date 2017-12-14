ab964c4850084a11d76bbea2553e4562814be1c5
U-BFFT\broecked <broecked@LT556.bfft.de>
2017-12-14 15:02:26 +0100

    make runnable again

--- reqif2html.py
+++ reqif2html.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import pyreqif.rif
+import pyreqif.reqif
 import pyreqif.html
 import sys
 import os
 
 def main():
-    doc = pyreqif.rif.load(sys.argv[1])
+    doc = pyreqif.reqif.load(sys.argv[1])
     pyreqif.html.dump(doc,sys.argv[2], os.path.dirname(sys.argv[1]))
 
 main()
--- reqif2xlsx.py
+++ reqif2xlsx.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python2
-import pyreqif.rif
+import pyreqif.reqif
 import pyreqif.xlsx
 import sys
 import os
 
 def main():
-    doc = pyreqif.rif.load(sys.argv[1])
+    doc = pyreqif.reqif.load(sys.argv[1])
     pyreqif.xlsx.dump(doc,sys.argv[2], os.path.dirname(sys.argv[1]))
 
 main()
