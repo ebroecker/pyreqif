5bbb03978ff465d0251d5541bd6e3918884bcff5
Eduard Bröcker <eduard@gmx.de>
2019-10-01 13:45:44 +0200

    Update Example
    * runs with py3
    * use commandline argument as reqif-input
    
    should fix #4

--- example.py
+++ example.py
@@ -1,41 +1,46 @@
-#!/usr/bin/env python2
-
+#!/usr/bin/env python
+from __future__ import print_function
 import os.path
 import pyreqif.reqif
 import pyreqif.rif
+import sys
 
-
-filename = "/home/edu/Downloads/ReqIFStudio/workspace/reqifTest/Spec.reqif"
-basepath = os.path.dirname(filename)
 
 
 def workOnHierarch(asd, deepth = 0):
     for i in range(0, deepth):
-        print "  ",
-    print asd["ReqIF.ForeignID"],
-    print asd["ReqIF.Text"]
+        print ("  ", end="")
+    print (asd["ReqIF.ChapterName"], end="")
+    print (asd["ReqIF.Text"])
     if "attachments" in asd:
         for attachment in asd["attachments"]:
             absPath = os.path.abspath(attachment)
-            print absPath
-    for id,child in asd["children"].iteritems():
+            print (absPath)
+    for id,child in asd["children"].items():
         workOnHierarch(child, deepth+1)
 
 
+if len(sys.argv) < 2:
+    print("Usage {} some_reqif_file.reqif\n".format(sys.argv[0]))
+    print("prints Text and Chapter Information and also exports a RIF file")
+    sys.exit()
+
+filename = sys.argv[1]
+basepath = os.path.dirname(filename)
 myDoc = pyreqif.reqif.load(filename)
-f = open("bb.xml", "wb")
+f = open(os.path.splitext(filename)[0] + "_pyreqif_export.xml", "wb")
 pyreqif.rif.dump(myDoc,f)
 
 
-print "Specification Requirements:"
+print ("Specification Requirements:")
 specs = myDoc.asDict()
 for spec in specs:
   for req in spec:
-      print req["ReqIF.ForeignID"],
-      print req["ReqIF.Text"]
+      print (req["ReqIF.ChapterName"], end="")
+      print (req["ReqIF.Text"])
 
 
-print "Specification Requirements (hierarchical):"
+print ("Specification Requirements (hierarchical):")
 hierarchSpec = myDoc.asHierarchDict()
-for id,topElement in hierarchSpec[0].iteritems():
+for id,topElement in hierarchSpec[0].items():
     workOnHierarch(topElement)
