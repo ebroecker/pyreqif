672860c1af8696d1c5073e5c7b6eff055f797cf7
ebroecker <eduard@gmx.de>
2019-10-17 09:00:23 +0200

    refactor xlsx a bit

--- pyreqif/xlsx.py
+++ pyreqif/xlsx.py
@@ -5,6 +5,39 @@ from lxml import etree
 import pyreqif.extractOleData
 import xlsxwriter
 
+def write_excel_line(worksheet, item, row, cols, depth, basepath):
+    files = []
+
+    for col, value in item.items():
+        if col not in cols:
+            continue
+        if type(value) is not bytes:
+            value = value.encode("utf8")
+        if value is not None:
+            if b"<" in value and b">" in value:
+                try:
+                    tree = etree.parse(io.BytesIO(value))
+                    root = tree.getroot()
+                    for element in root.iter("object"):
+                        rtfFilename = os.path.join(basepath, element.attrib["data"])
+                        files = pyreqif.extractOleData.extractOleData(rtfFilename)
+
+                        if len(files) > 0:
+                            for key in element.attrib:
+                                del element.attrib[key]
+                            element.tag = "a"
+                            element.set("href", files[0])
+                            element.text = "linked file: " + files[0]
+                    value = "".join(root.itertext())
+                    value = value.encode("utf8")
+                except:
+                    pass
+        worksheet.write(row, cols.index(col), value.decode("utf-8"))
+        worksheet.set_row(row, None, None, {'level': depth})
+        if len(files) > 0:
+            worksheet.insert_image(row, cols.index(col), os.path.splitext(files[0])[0] + ".png")
+
+
 def dump(myDoc, outfile, basepath = None):
     if basepath is None:
         basepath = os.path.dirname(outfile)
@@ -22,35 +55,6 @@ def dump(myDoc, outfile, basepath = None):
 
     for child in myDoc.hierarchy:
         for item, depth in  myDoc.hierach_iterator(child, cols):
-            files = []
             row += 1
-            for col, value in item.items():
-                if col not in cols:
-                    continue
-                if type(value) is not bytes:
-                    value = value.encode("utf8")
-                if value is not None:
-                    if b"<" in value and b">" in value:
-                        try:
-                            tree = etree.parse(io.BytesIO(value))
-                            root = tree.getroot()
-                            for element in root.iter("object"):
-                                rtfFilename = os.path.join(basepath, element.attrib["data"])
-                                files = pyreqif.extractOleData.extractOleData(rtfFilename)
-
-                                if len(files) > 0:
-                                    for key in element.attrib:
-                                        del element.attrib[key]
-                                    element.tag = "a"
-                                    element.set("href", files[0])
-                                    element.text = "linked file: " + files[0]
-                            value = "".join(root.itertext())
-                            value = value.encode("utf8")
-                        except:
-                            pass
-                worksheet.write(row, cols.index(col), value.decode("utf-8"))
-                worksheet.set_row(row, None, None, {'level': depth})
-                if len(files) > 0:
-                    worksheet.insert_image(row, cols.index(col), os.path.splitext(files[0])[0] + ".png")
-
+            write_excel_line(worksheet, item, row, cols, depth, basepath)
     workbook.close()
