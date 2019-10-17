5f126aa41f5ddb3021d5c957575592a66e475562
ebroecker <eduard@gmx.de>
2019-10-17 09:25:19 +0200

    update example to use hierarch_iterator update html exporter to use hierarch_iterator #9

--- pyreqif/html.py
+++ pyreqif/html.py
@@ -9,13 +9,65 @@ import sys
 #reload(sys)
 #sys.setdefaultencoding('utf8')
 
+def create_html(item, cols, depth, basepath):
+    tempReq = {}
+    htmlOutput = "<td>"
+    htmlOutput += "+" * depth
+
+    for col, value in item.items():
+        if col not in cols:
+            continue
+        if type(value) is not bytes:
+            value = value.encode("utf8")
+        if value is not None:
+            if b"<" in value and b">" in value:
+                try:
+                    tree = etree.parse(io.BytesIO(value))
+                    root = tree.getroot()
+                    for element in root.iter("object"):
+                        rtfFilename = os.path.join(basepath, element.attrib["data"])
+                        files = pyreqif.ole2rtf.ole2rtf(rtfFilename)
+                        if rtfFilename.endswith("ole"):
+                            if len(files) > 0:
+                                if "name" in element.attrib:
+                                    name = element.attrib["name"]
+                                else:
+                                    name = ""
+
+                                for key in element.attrib:
+                                    del element.attrib[key]
+                                element.tag = "a"
+                                element.set("href", files[0])
+                                element.text = name
+                                imgElement = etree.Element("img")
+                                imgElement.set("src", os.path.splitext(files[0])[0] + ".png")
+                                element.append(imgElement)
+                            else:
+                                element.tag = "div"
+                        else:
+                            element.tag = "div"
+                    value = etree.tostring(root)
+                    value = value.encode("utf8")
+                except:
+                    pass
+        tempReq[col] = value
+    for col in cols:
+        htmlOutput += "<td>"
+        if col in tempReq:
+            if tempReq[col] is not None:
+                htmlOutput += tempReq[col].decode("utf8")
+            else:
+                htmlOutput += ""
+    htmlOutput += "</tr><tr>"
+    return htmlOutput
+
 
 def dump(myDoc, outfile, basepath = None):
     if basepath is None:
         basepath = os.path.dirname(outfile)
 
     for specification in myDoc.specificationList:
-        htmlOutput = u"<table border=1><tr>"
+        htmlOutput = u"<table border=1><tr><td>"
         cols = myDoc.fields
 
         colNr = 0
@@ -25,50 +77,12 @@ def dump(myDoc, outfile, basepath = None):
 
         htmlOutput += u"</tr><tr>"
 
-        for req in specification:
-            reqObj = myDoc.getReqById(req)
-            tempReq ={}
-#            for col,value in myDoc.flatReq(reqObj, html=True).iteritems():
-            for col,value in myDoc.flatReq(reqObj, html=True).items():
-                if value is not None:
-                    if "<" in str(value):
-                        try:
-                            tree = etree.parse(io.BytesIO(value))
-                            root = tree.getroot()
-                            for element in root.iter("object"):
-                                rtfFilename = os.path.join(basepath, element.attrib["data"])
-                                files = pyreqif.ole2rtf.ole2rtf(rtfFilename)
-                                if rtfFilename.endswith("ole"):
-                                    if len(files) > 0:
-                                        if "name" in element.attrib:
-                                            name = element.attrib["name"]
-                                        else:
-                                            name = ""
-
-                                        for key in element.attrib:
-                                            del element.attrib[key]
-                                        element.tag = "a"
-                                        element.set("href", files[0])
-                                        element.text = name
-                                        imgElement = etree.Element("img")
-                                        imgElement.set("src", os.path.splitext(files[0])[0] + ".png")
-                                        element.append(imgElement)
-                                    else:
-                                        element.tag = "div"
-                                else:
-                                    element.tag = "div"
-                            value = etree.tostring(root)
-                        except:
-                            pass
-                tempReq[col] = value
-            for col in cols:
-                htmlOutput += "<td>"
-                if col in tempReq:
-                    if tempReq[col] is not None:
-                        htmlOutput += str(tempReq[col])
-                    else:
-                        htmlOutput += ""
-            htmlOutput += "</tr><tr>"
+        for child in myDoc.hierarchy:
+            for item, depth in myDoc.hierach_iterator(child, cols):
+                htmlOutput += create_html(item, cols, depth, basepath)
+#        for req in specification:
+#            reqObj = myDoc.getReqById(req)
+#            htmlOutput += create_html(myDoc.flatReq(reqObj, html=True), cols, 0, basepath)
         htmlOutput += "</table>"
     fp = open(outfile, "wb")
     fp.write(bytes(htmlOutput, "utf8"))
