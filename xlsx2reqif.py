4dd0cbd3def802335bb8465db3e237c109327089
Joost Sannen <50947544+sannenj@users.noreply.github.com>
2023-01-09 10:17:59 +0100

    Fix for issue 32 (#37)
    * Update xlsx2reqif.py
    
    Fix incorrect translation of < character to &gt
    
    * Fix replacing '<' properly
    
    Thanks @sannenj and @olbermann

--- xlsx2reqif.py
+++ xlsx2reqif.py
@@ -114,8 +114,8 @@ for row_nr in range(2, ws.max_row+1):
         # do images:
         pictures = get_images(images, row_nr - 1, columns.index(col))
         if type(xls_req[col]) == str:
-            xls_req[col] = xls_req[col].replace("<", "&gt;")
-            xls_req[col].replace("<", "&lt;")
+            xls_req[col] = xls_req[col].replace(">", "&gt;")
+            xls_req[col] = xls_req[col].replace("<", "&lt;")
         if len(pictures) > 0:
             for pic in pictures:
                 xls_req[col] = "" if xls_req[col] is None else xls_req[col]
